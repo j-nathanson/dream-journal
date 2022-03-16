378cf65a24d1b8876957e7396995bcd23f6914d1
jacobnathanson <jacobnath93@gmail.com>
2022-03-16 16:48:11 -0400

    get all customers

--- client/src/components/customers/Customers.js
+++ client/src/components/customers/Customers.js
@@ -1,12 +1,25 @@
-import React from 'react'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
 import CustomerForm from './CustomerForm'
 import CustomerList from './CustomerList'
 
 export default function Customers() {
+    const empty = []
+    const [customers, setCustomers] = useState(empty)
+
+    // get all customers from db, set local array
+    const getCustomers = async () => {
+        const customersRes = await axios.get('http://localhost:3001/customer')
+        setCustomers(customersRes.data)
+    }
+
+    useEffect(() => {
+        getCustomers()
+    }, [])
     return (
         <div>
             <CustomerForm />
-            <CustomerList />
+            <CustomerList customer={customers} />
         </div>
     )
 }
